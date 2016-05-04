ac6d1b6c50e0777b554a327045132643a54653c5
Shuaiji Qian <shuaijiqian@shuaijis-imac.local>
2016-05-04 23:45:36 +1000

    add a arrow image

---
+++ Mfmproject/AddressButton.swift
@@ -0,0 +1,30 @@
+//
+//  AddressButton.swift
+//  Mfmproject
+//
+//  Created by Shuaiji Qian on 4/05/2016.
+//  Copyright © 2016 Tony Qian. All rights reserved.
+//
+
+import UIKit
+
+class AddressButton: UIButton {
+    
+    
+    override func awakeFromNib() {
+//        let image = UIImage(named: "arrow.jpg") as UIImage?
+//        self.setBackgroundImage(image, forState: UIControlState.Normal)
+        
+        layer.cornerRadius = 5.0
+       
+    }
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
